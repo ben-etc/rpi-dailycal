1753064de6c3f89ddab22ac3155764f004e3d63c
Ben Mooney <46793547+ben-etc@users.noreply.github.com>
2019-01-23 17:55:23 -0800

    Added loop to find secretPixel on grayscale screens. Added multiple location modes, including a manual placement mode. Modified convertBitmap to no longer nest the datebox creation within it; it is now its own function and has been renamed to addDateboxes.

--- createBitmaps.py
+++ createBitmaps.py
@@ -3,14 +3,31 @@
 from PIL import Image, ImageFont, ImageColor, ImageDraw
 import datetime
 
-def convertBitmap(RGBImage, screencolor = "red"):
+def convertBitmap(RGBImage, screencolor, datelocation = "detect", datecoords = (0,0)):
     imgwidth = RGBImage.width
     imgheight = RGBImage.height
     if screencolor != "grayscale":
         colorscale = Image.new("L", (imgwidth,imgheight), 255)  # Create an empty grayscale image for the color channel
     grayscale = Image.new("L", (imgwidth,imgheight), 255)  # Create an empty grayscale image that will be the black channel
     secret = (0, 0, 255, 255)
-    secretPixel = (0,0) # This will be used to figure out where the calendar will be placed on the image
+    
+    # The logic for everything but topright will put the box out of bounds, but this is corrected in addDateboxes once the box is dynamically generated.
+    if datelocation == "detect":
+        secretPixel = (-1,-1) # This will be used to figure out where the calendar will be placed on the image
+    elif datelocation == "topleft":
+        secretPixel = (0, 0)
+    elif datelocation == "bottomleft":
+        secretPixel = (0, imgheight)
+    elif datelocation == "topright":
+        secretPixel = (imgwidth, 0)
+    elif datelocation == "bottomright":
+        secretPixel = (imgwidth, imgheight)
+    elif datelocation == "manual":
+        secretPixel = datecoords
+    else:
+        print("Date location {} is not a valid choice. Defaulting to topleft".format(datelocation))
+        secretPixel = (0,0)
+    
     
     mode = RGBImage.mode
     
@@ -29,7 +46,7 @@ def convertBitmap(RGBImage, screencolor = "red"):
                     inverseAlpha = 255 - a
                     r, g, b = (r + inverseAlpha), (g + inverseAlpha), (b + inverseAlpha)
 
-                if (r, g, b, a) == secret and secretPixel == (0,0):
+                if (r, g, b, a) == secret and secretPixel == (-1,-1):
                     secretPixel = (x,y)
 
                 # Math for red screens
@@ -50,27 +67,33 @@ def convertBitmap(RGBImage, screencolor = "red"):
         # Convert the two grayscale images to single color bitmaps
         colorbitmap = colorscale.convert("1")
         blackbitmap = grayscale.convert("1")
-        
-        # Create the calendar graphic and paste it onto each bitmap.
-        blackdate, colordate = getDateboxes(screencolor)
-        colorbitmap.paste(colordate, secretPixel)
-        blackbitmap.paste(blackdate, secretPixel)
     
     elif screencolor == "grayscale":
         blackbitmap = RGBImage.convert("1")
-        colorbitmap = ""
+        colorbitmap = "" # colorbitmap needs a dummy value to return
+        # If the date location is "detect", we still need to parse each pixel to find the secret pixel.
+        # I realize this is repeating an awful lot of code up above. This should be refacroted later.
+        if datelocation == "detect":
+            for y in range(imgheight):
+                for x in range(imgwidth):
+                    if mode == "RGB":
+                        r, g, b = RGBImage.getpixel((x,y))
+                        a = 255
+                    elif mode == "RGBA":
+                        r, g, b, a = RGBImage.getpixel((x,y))
+                    
+                    if (r, g, b, a) == secret and secretPixel == (-1, -1):
+                        secretPixel = (x,y)
         
-        blackdate, colordate = getDateboxes(screencolor, border = True, margin = 5)
-        blackbitmap.paste(blackdate, secretPixel)
-        
-        # To do: grayscale needs to still look for the secret pixel and add the calendar.
     else:
         print("{} is not a valid screencolor!".format(screencolor))
         # Return dummy values
         blackbitmap = ""
         colorbitmap = ""
+    if secretPixel == (-1, -1):
+        secretPixel = (0, 0) # Default the calendar to 0,0 if it hasn't been detected.
     print("Secret pixel: {}".format(secretPixel))
-    return blackbitmap, colorbitmap
+    return blackbitmap, colorbitmap, secretPixel
 
 # This function is largely used for testing. It will save a file a combined output to use as a preview
 def mergeBitmaps(blackbitmap, colorbitmap, screencolor = "red"):
@@ -94,7 +117,7 @@ def mergeBitmaps(blackbitmap, colorbitmap, screencolor = "red"):
     
     return outputImage
 
-def getDateboxes(screencolor, border = True, margin = 5):
+def addDateboxes(screencolor, blackbitmap, colorbitmap, secretPixel, border = True, margin = 5, fontname = "LeagueSpartan-Bold.otf"):
     today = datetime.datetime.now()
     weekdays = {
         0: "Monday",
@@ -122,7 +145,7 @@ def getDateboxes(screencolor, border = True, margin = 5):
     }
     
     indent = margin + 20
-    font = ImageFont.truetype("./fonts/LeagueSpartan-Bold.otf", 32)
+    font = ImageFont.truetype("./fonts/{}".format(fontname), 32)
     dayofweek = weekdays[today.weekday()]
     formattedDate = "{} {}".format(months[today.month], today.day)
     width = max( (margin * 2 + font.getsize(dayofweek)[0]), (margin + indent + font.getsize(formattedDate)[0]))
@@ -130,6 +153,10 @@ def getDateboxes(screencolor, border = True, margin = 5):
     height = newline + margin + font.getsize(formattedDate)[1]
     blackdate = Image.new("1", (width, height), 1)
     blackDraw = ImageDraw.Draw(blackdate)
+    # Recalculate the secretPixel to avoid going out of bounds
+    x = min(blackbitmap.width - width, secretPixel[0])
+    y = min(blackbitmap.height - height, secretPixel[1])
+    secretPixel = (x,y)
     if screencolor != "grayscale":
         colordate = Image.new("1", (width, height), 1)
         colorDraw = ImageDraw.Draw(colordate)
@@ -141,12 +168,13 @@ def getDateboxes(screencolor, border = True, margin = 5):
         blackDraw.text((margin - 2, margin + 2), dayofweek, fill = 0, font = font) # Shadow
         blackDraw.text((margin, margin), dayofweek, fill = 1, font = font)
         blackDraw.text((indent, newline), formattedDate, fill = 0, font = font)
+        colorbitmap.paste(colordate, secretPixel)
+        blackbitmap.paste(blackdate, secretPixel)
 
     elif screencolor == "grayscale":
         if border == True:
             blackDraw.rectangle((0,0, blackdate.width, blackdate.height), fill = 1, outline = 0, width = 2)
         blackDraw.text((margin, margin), dayofweek, fill = 0, font = font)
         blackDraw.text((indent, newline), formattedDate, fill = 0, font = font)
-        colordate = "" # Dummy data for the return at end of function.
-
-    return blackdate, colordate
+        blackbitmap.paste(blackdate, secretPixel)
+    return blackbitmap, colorbitmap
